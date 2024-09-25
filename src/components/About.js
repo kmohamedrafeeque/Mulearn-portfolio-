46b9dafa2752bd3d9a63c52742e164d7e0a02c15
K Mohamed Rafeeque <125984054+kmohamedrafeeque@users.noreply.github.com>
2024-09-25 20:59:02 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -7,13 +7,13 @@ export default function About() {
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Hi, I'm alan.
+            Hi, I'm Rafeeque.
             <br className="hidden lg:inline-block" />I love to build amazing
             apps,web,robots,etc.
           </h1>
           <p className="mb-8 leading-relaxed">
-          Hello, my name is Alan, and I'm a  BCA student at Mar Augusthinose College, 
-          Ramapuram, from Rampuram, Kottayam. I have a passion for writing programs,
+          Hello, my name is Rafeeque, and I'm a  B.Tech student at  AWH Engineering College, 
+          Kuttikkattoor , from Kuttikkattoor, Kozhikode. I have a passion for writing programs,
            building robots, creating websites, designing apps, exploring AI, music, photography, videography,
             and editing.
              My dream is to establish a technology-based company that harnesses the power of innovation to make 
